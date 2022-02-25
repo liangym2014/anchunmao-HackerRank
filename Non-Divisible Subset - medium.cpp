d95fc57292ae380f82ca4e16b00561d92e933110
anchunmao <79773229+liangym2014@users.noreply.github.com>
2022-02-24 17:03:34 -0800

    Create Non-Divisible Subset - medium.cpp

---
+++ Non-Divisible Subset - medium.cpp
@@ -0,0 +1,17 @@
+// Given a set of distinct integers, print the size of a maximal subset of S where the sum of any 2 numbers in S' is not evenly divisible by k.
+int nonDivisibleSubset(int k, vector<int> s) {
+    vector<int> rem(k);
+    for(auto &n: s)
+        rem[n % k] ++;
+
+    if(rem[0] > 1)
+        rem[0] = 1;
+    
+    int ans = rem[0];    
+    for(int l = 1, r = k - 1; l < r; l ++, r --)
+        ans += max(rem[l], rem[r]);
+    
+    if((k & 1) == 0 && rem[k >> 1] >= 1)
+        ans += 1;
+    return ans;
+}
